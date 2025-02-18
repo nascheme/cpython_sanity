8a25cbf226053db3a7a3c71484b228c4d067f4fa
Neil Schemenauer <nas@arctrix.com>
2025-02-18 11:09:50 -0800

    Add Dockerfile to build scipy.

--- build-all.sh
+++ build-all.sh
@@ -4,3 +4,4 @@ set -eu
 
 DOCKER_BUILDKIT=1 docker build --progress=plain --tag cpython-tsan -f Dockerfile.cpython .
 DOCKER_BUILDKIT=1 docker build --progress=plain --tag numpy-tsan -f Dockerfile.numpy .
+DOCKER_BUILDKIT=1 docker build --progress=plain --tag scipy-tsan -f Dockerfile.scipy .
